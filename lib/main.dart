3ad4995c42d931f5e3bc067a2a0f1e195a68a334
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2024-03-25 18:52:09 +0700

    clear routes file

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import  'pages/index.dart';
+import 'package:studenthub/components/radiolisttypes.dart';
+import 'package:studenthub/data/test/data_project.dart';
+import 'package:studenthub/pages/chat_flow/chat_screen.dart';
+import 'package:studenthub/pages/company_review_proposal/project_proposal_list.dart';
+import 'package:studenthub/pages/home_page.dart';
+import 'package:studenthub/pages/signin_page.dart';
+import 'package:studenthub/pages/signupinfo_page.dart';
+import 'package:studenthub/pages/signuptype_page.dart';
+import 'pages/index.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
@@ -20,23 +28,41 @@ class StudentHub extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: ProjectProposalListScreen(project: dataProject[0]),
+      home: HomePage(),
+      initialRoute: '/',
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case '/company':
-            return MaterialPageRoute(builder: (context) => const CompanyProfileInputScreen());
+            return MaterialPageRoute(
+                builder: (context) => const CompanyProfileInputScreen());
           case '/company/profile':
-            return MaterialPageRoute(builder: (context) => const CompanyProfileInputScreen());
+            return MaterialPageRoute(
+                builder: (context) => const CompanyProfileInputScreen());
           case '/company/welcome':
-            return MaterialPageRoute(builder: (context) => const CompanyWelcomeScreen());
+            return MaterialPageRoute(
+                builder: (context) => const CompanyWelcomeScreen());
           case '/company/dashboard':
-            return MaterialPageRoute(builder: (context) => const CompanyDashboardScreen());
+            return MaterialPageRoute(
+                builder: (context) => const CompanyDashboardScreen());
           case '/student':
-            return MaterialPageRoute(builder: (context) => const StudentProfileInputScreen1());
+            return MaterialPageRoute(
+                builder: (context) => const StudentProfileInputScreen1());
           case '/student/profileinput1':
-            return MaterialPageRoute(builder: (context) => const StudentProfileInputScreen1());
+            return MaterialPageRoute(
+                builder: (context) => const StudentProfileInputScreen1());
           case '/chatter':
             return MaterialPageRoute(builder: (context) => const ChatScreen());
+          case '/signin':
+            return MaterialPageRoute(builder: (context) => const SigninPage());
+          case '/signup/step1':
+            return MaterialPageRoute(
+                builder: (context) => const SignupTypePage());
+          case '/signup/step2':
+            MaterialPageRoute(builder: (context, {arguments}) {
+              // Extract the type parameter from the arguments
+              final AccountTypes type = AccountTypes.company;
+              return SignupInfoPage(selectedType: type);
+            });
           default:
             return null;
         }
@@ -44,4 +70,3 @@ class StudentHub extends StatelessWidget {
     );
   }
 }
-
--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,16 +1,3 @@
 import 'package:flutter/material.dart';
-import 'package:studenthub/components/radiolisttypes.dart';
-import 'package:studenthub/pages/signin_page.dart';
-import 'package:studenthub/pages/signupinfo_page.dart';
-import 'package:studenthub/pages/signuptype_page.dart';
 
-var customRoutes = <String, WidgetBuilder>{
-  '/': (context) => SigninPage(),
-  '/signin': (context) => SigninPage(),
-  '/signup/step1': (context) => SignupTypePage(),
-  '/signup/:type': (context, {arguments}) {
-    // Extract the type parameter from the arguments
-    final AccountTypes type = AccountTypes.company;
-    return SignupInfoPage(selectedType: type);
-  },
-};
+var customRoutes = <String, WidgetBuilder>{};
