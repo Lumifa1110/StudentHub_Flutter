67b2b5f98841181fa5885558ff000a5913364e06
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2024-04-21 18:11:30 +0700

    Update company_dashboard.dart

--- lib/screens/company/company_dashboard.dart
+++ lib/screens/company/company_dashboard.dart
@@ -252,23 +252,26 @@ class CompanyDashboardScreenState extends State<CompanyDashboardScreen> {
       );
       final responseDecode = jsonDecode(responseJson.body)["result"];
 
-      listProjectArchived.clear();
-      for (int i = 0; i < responseDecode.length; i++) {
-        listProjectArchived.add(Project(
-            projectId: responseDecode[i]['id'],
-            createdAt: responseDecode[i]['createdAt'],
-            updatedAt: responseDecode[i]['updatedAt'],
-            deletedAt: responseDecode[i]['deletedAt'],
-            companyId: responseDecode[i]['companyId'],
-            companyName: responseDecode[i]['companyName'],
-            projectScopeFlag: responseDecode[i]['projectScopeFlag'],
-            title: responseDecode[i]['title'],
-            description: responseDecode[i]['description'],
-            numberOfStudents: responseDecode[i]['numberOfStudents'],
-            typeFlag: responseDecode[i]['typeFlag'],
-            countProposals: responseDecode[i]['countProposals'],
-            isFavorite: responseDecode[i]['isFavorite']));
+      if (responseDecode != null) {
+        listProjectArchived.clear();
+        for (int i = 0; i < responseDecode.length; i++) {
+          listProjectArchived.add(Project(
+              projectId: responseDecode[i]['id'],
+              createdAt: responseDecode[i]['createdAt'],
+              updatedAt: responseDecode[i]['updatedAt'],
+              deletedAt: responseDecode[i]['deletedAt'],
+              companyId: responseDecode[i]['companyId'],
+              companyName: responseDecode[i]['companyName'],
+              projectScopeFlag: responseDecode[i]['projectScopeFlag'],
+              title: responseDecode[i]['title'],
+              description: responseDecode[i]['description'],
+              numberOfStudents: responseDecode[i]['numberOfStudents'],
+              typeFlag: responseDecode[i]['typeFlag'],
+              countProposals: responseDecode[i]['countProposals'],
+              isFavorite: responseDecode[i]['isFavorite']));
+        }
       }
+
       if (mounted) {
         // Check again if the widget is still mounted before calling setState
         setState(() {
