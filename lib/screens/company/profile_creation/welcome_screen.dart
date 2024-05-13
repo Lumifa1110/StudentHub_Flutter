4e89d5dcb1a441095ce665e6f2982a936e7a2bad
Lumifa1110 <lmphat20@clc.fitus.edu.vn>
2024-05-13 18:27:02 +0700

    Update welcome_screen.dart

--- lib/screens/company/profile_creation/welcome_screen.dart
+++ lib/screens/company/profile_creation/welcome_screen.dart
@@ -9,66 +9,50 @@ class CompanyWelcomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Row(
-          children: [
-            Expanded(
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Student Hub',
-                  style: TextStyle(
-                    color: Color(0xffffffff),
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold
+        appBar: AppBar(
+          title: const Row(
+            children: [
+              Expanded(
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Student Hub',
+                    style: TextStyle(
+                        color: Color(0xffffffff), fontSize: 20, fontWeight: FontWeight.bold),
                   ),
                 ),
               ),
-            ),
-            FaIcon(
-              FontAwesomeIcons.solidUser,
-              color: Color(0xffffffff),
-              size: 24
-            )
-          ],
+              FaIcon(FontAwesomeIcons.solidUser, color: Color(0xffffffff), size: 24)
+            ],
+          ),
+          backgroundColor: Colors.blue,
         ),
-        backgroundColor: Colors.blue,
-      ),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-          child: Column (
-            children: [
-              // TEXT: Welcome
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      width: double.infinity,
-                      margin: const EdgeInsets.only(top: 40, bottom: 20),
-                      padding: const EdgeInsets.symmetric(vertical: 10),
-                      alignment: Alignment.topCenter,
-                      child: const Text('Welcome to Student Hub, Company!\nLet\'s start with your first project post!',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.normal
-                        ),
-                      )
+        body: SingleChildScrollView(
+            child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: Column(children: [
+                  // TEXT: Welcome
+                  Row(children: [
+                    Expanded(
+                      child: Container(
+                          width: double.infinity,
+                          margin: const EdgeInsets.only(top: 40, bottom: 20),
+                          padding: const EdgeInsets.symmetric(vertical: 10),
+                          alignment: Alignment.topCenter,
+                          child: const Text(
+                            'Welcome to Student Hub, Company!\nLet\'s start with your first project post!',
+                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                          )),
                     ),
-                  ),
-                ]
-              ),
-              ButtonSimple(
-                label: 'Get started!', 
-                onPressed: () => {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => const CompanyDashboardScreen()))
-                },
-                isButtonEnabled: true,
-              )
-            ]
-          )
-        )
-      )
-    );
+                  ]),
+                  ButtonSimple(
+                    label: 'Get started!',
+                    onPressed: () => {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => const CompanyDashboardScreen()))
+                    },
+                    isButtonEnabled: true,
+                  )
+                ]))));
   }
 }
